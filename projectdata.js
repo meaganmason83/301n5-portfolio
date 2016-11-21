68310f2b35a16f33d4b0e13c67dc381b6c22f136
Meagan Mason <meagananne@Meagans-MacBook-Pro.local>
2016-11-20 21:21:35 -0800

    fixing underscore on contact icons

--- projectdata.js
+++ projectdata.js
@@ -2,16 +2,16 @@
 
 var localData = [
   {
+    title: 'Home Field Advantage',
+    url: 'https://rtuts67.github.io/Final_Project_HFA_201n6/',
+    iframe: 'https://rtuts67.github.io/Final_Project_HFA_201n6/',
+    language: 'HTML/CSS/JavaScript',
+    description: '<p>Group Project</p>'
+  },
+  {
     title: 'Campfire Coffee',
     url: 'https://meaganmason83.github.io/class_8_lab/',
     iframe: 'https://meaganmason83.github.io/class_8_lab/',
-    language: 'HTML/CSS/JavaScript',
-    description: '<p>Individual Project</p>'
-  },
-  {
-    title: 'Chocolate Pizza',
-    url: 'https://meaganmason83.github.io/ChocoPizza/',
-    iframe: 'https://meaganmason83.github.io/ChocoPizza/',
     language: 'HTML/CSS/JavaScript',
     description: '<p>Individual Project</p>'
   },
@@ -23,10 +23,10 @@ var localData = [
     description: '<p>Individual Project</p>'
   },
   {
-    title: 'Home Field Advantage',
-    url: 'https://rtuts67.github.io/Final_Project_HFA_201n6/',
-    iframe: 'https://rtuts67.github.io/Final_Project_HFA_201n6/',
-    language: 'HTML/CSS/JavaScript',
-    description: '<p>Group Project</p>'
+    title: 'Chocolate Pizza',
+    url: 'https://meaganmason83.github.io/ChocoPizza/',
+    iframe: 'https://meaganmason83.github.io/ChocoPizza/',
+    language: 'HTML/CSS',
+    description: '<p>Individual Project</p>'
   }
 ];
