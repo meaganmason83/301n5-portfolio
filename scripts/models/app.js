84e6fe7c500285f06aa3ccd504d9f352859fbea8
Meagan Mason <meagananne@Meagans-MacBook-Pro.local>
2016-12-03 13:56:43 -0800

    adding IIFE, still trying to get portfolio methods loadAll and fetchAll working

--- scripts/models/app.js
+++ scripts/models/app.js
@@ -1,44 +1,42 @@
 'use strict';
 
-function Portfolio(opts) {
-  for (let keys in opts) {
-    this[keys] = opts[keys];
-  // this.title = opts.title;
-  // this.url = opts.url;
-  // this.iframe = opts.iframe;
-  // this.language = opts.language;
-  // this.description = opts.description;
+(function(module) {
+
+  function Portfolio(opts) {
+    for (let keys in opts) {
+      this[keys] = opts[keys];
+    }
   }
-}
 
-Portfolio.allPortfolios = [];
+  Portfolio.allPortfolios = [];
 
-Portfolio.prototype.toHtml = function() {
-  var htmlTemp = Handlebars.compile($('#handlebarsTemplate').html());
-  $('#myPortfolios').append(htmlTemp(this));
-};
+  Portfolio.prototype.toHtml = function() {
+    var htmlTemp = Handlebars.compile($('#handlebarsTemplate').html());
+    $('#myPortfolios').append(htmlTemp(this));
+  };
 
-//combining push and append
-// Portfolio.loadAll = function(inputData) {
-//   inputData.forEach(function(theCurrentArticleObject) {
-//     Portfolio.allPortfolios.push(new Portfolio(theCurrentArticleObject));
-//   }).forEach(function() {
-//     $('#myPortfolios').append(Portfolio.allPortfolios.toHtml());
-//   })
-// };
+// combining push and append
+  Portfolio.loadAll = function(inputData) {
+    inputData.forEach(function(theCurrentArticleObject) {
+      Portfolio.allPortfolios.push(new Portfolio(theCurrentArticleObject));
+    }).forEach(function() {
+      $('#myPortfolios').append(Portfolio.allPortfolios.toHtml());
+    })
+  };
 
-Portfolio.fetchAll = function() {
-  if (localStorage.localdata) {
-    var localData = JSON.parse(localStorage.getItem('localdata'));
-    Portfolio.loadAll(localData);
-    portfolioView.handleMainNav();
-  } else {
-    $.getJSON('data/localdata.json', function(data) {
-      localStorage.setItem('localdata', JSON.stringify(data));
+  Portfolio.fetchAll = function() {
+    if (localStorage.localdata) {
       var localData = JSON.parse(localStorage.getItem('localdata'));
       Portfolio.loadAll(localData);
       portfolioView.handleMainNav();
-    });
-  }
-};
-// Portfolio.fetchAll();
+    } else {
+      $.getJSON('data/localdata.json', function(data) {
+        localStorage.setItem('localdata', JSON.stringify(data));
+        var localData = JSON.parse(localStorage.getItem('localdata'));
+        Portfolio.loadAll(localData);
+        portfolioView.handleMainNav();
+      });
+    }
+  };
+  module.Portfolio = Portfolio;
+})(window);
--- scripts/views/portfolioView.js
+++ scripts/views/portfolioView.js
@@ -1,28 +1,31 @@
 'use strict';
 
-var portfolioView = {};
+(function(module) {
 
-portfolioView.handleMainNav = function () {
-  $('#icons').hide();
-  $('#myPortfolios').hide();
-  $('#aboutme').hide();
-  $('.main-nav').on('click', '.tab', function() {
-    if ($(this).data('navbar') === 'portfolio') {
-      $('#myPortfolios').fadeIn(1000);
-      $('#icons').hide();
-      $('#aboutme').hide();
-    } else if ($(this).data('navbar') === 'contact') {
-      $('#icons').fadeIn(1000);
-      $('#aboutme').hide();
-      $('#myPortfolios').hide();
-    } else if ($(this).data('navbar') === 'about') {
-      $('#aboutme').fadeIn(1000);
-      $('#icons').hide();
-      $('#myPortfolios').hide();
-    }
-  });
-  $('.main-nav .tab:first').click();
-};
+  var portfolioView = {};
 
-portfolioView.handleMainNav();
-// Portfolio.fetchAll();
+  portfolioView.handleMainNav = function () {
+    $('#icons').hide();
+    $('#myPortfolios').hide();
+    $('#aboutme').hide();
+    $('.main-nav').on('click', '.tab', function() {
+      if ($(this).data('navbar') === 'portfolio') {
+        $('#myPortfolios').fadeIn(1000);
+        $('#icons').hide();
+        $('#aboutme').hide();
+      } else if ($(this).data('navbar') === 'contact') {
+        $('#icons').fadeIn(1000);
+        $('#aboutme').hide();
+        $('#myPortfolios').hide();
+      } else if ($(this).data('navbar') === 'about') {
+        $('#aboutme').fadeIn(1000);
+        $('#icons').hide();
+        $('#myPortfolios').hide();
+      }
+    });
+    $('.main-nav .tab:first').click();
+  };
+  Portfolio.fetchAll(portfolioView.handleMainNav);
+  portfolioView.handleMainNav();
+  module.portfolioView = portfolioView;
+})(window);
