a1d11ef37b3d51f3a71fbfd45109211a834db50c
Meagan Mason <meagananne@Meagans-MacBook-Pro.local>
2016-12-17 13:16:19 -0800

    removing main nav function from portfolioView

--- scripts/views/portfolioView.js
+++ scripts/views/portfolioView.js
@@ -4,28 +4,6 @@
 
   var portfolioView = {};
 
-  // portfolioView.handleMainNav = function () {
-  //   $('#icons').hide();
-  //   $('#myPortfolios').hide();
-  //   $('#aboutme').hide();
-  //   $('.main-nav').on('click', '.tab', function() {
-  //     if ($(this).data('navbar') === 'portfolio') {
-  //       $('#myPortfolios').fadeIn(1000);
-  //       $('#icons').hide();
-  //       $('#aboutme').hide();
-  //     } else if ($(this).data('navbar') === 'contact') {
-  //       $('#icons').fadeIn(1000);
-  //       $('#aboutme').hide();
-  //       $('#myPortfolios').hide();
-  //     } else if ($(this).data('navbar') === 'about') {
-  //       $('#aboutme').fadeIn(1000);
-  //       $('#icons').hide();
-  //       $('#myPortfolios').hide();
-  //     }
-  //   });
-  //   $('.main-nav .tab:first').click();
-  // };
-
   Portfolio.renderAll = function(inputData) {
     Portfolio.allPortfolios.forEach(function(allPortfolios) {
       $('#myPortfolios').append(allPortfolios.toHtml());
@@ -33,7 +11,6 @@
   }
 
   Portfolio.fetchAll();
-  // portfolioView.handleMainNav();
 
   module.portfolioView = portfolioView;
 })(window);
